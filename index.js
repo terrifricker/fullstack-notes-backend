feda8d9bada5fbf2c7236d9abf9dfb65c15f98e4
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2024-02-27 13:12:21 -0800

    add route for adding a note

--- index.js
+++ index.js
@@ -47,6 +47,33 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
