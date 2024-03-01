99de8dcacf16daa1eb5cfa892ceb025370cdb8e9
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2024-03-01 12:05:29 -0800

    give database the name 'noteApp' by adding it to the url

---
+++ mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@fricker-fullstack-notes.oafm7os.mongodb.net/noteApp?retryWrites=true&w=majority&appName=fricker-fullstack-notes`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
