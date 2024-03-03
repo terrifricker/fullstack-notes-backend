e058dac87452b371ec6e7e3d5d587321fa3702c4
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2024-03-02 22:15:51 -0800

    move mongoose code into a node module found in ./models/note.js

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
+const Note = require('./models/note')
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-const password = process.argv[2]
-
-const url =
-  `mongodb+srv://fullstack:${password}@fricker-fullstack-notes.oafm7os.mongodb.net/noteApp?retryWrites=true&w=majority&appName=fricker-fullstack-notes`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Note = mongoose.model('Note', noteSchema)
 
 let notes = [
   {
---
+++ models/note.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
